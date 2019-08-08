57fd4a44f80892c12315b3335d29c361128ec150
anuttara88 <53886252+anuttara88@users.noreply.github.com>
2019-08-08 10:27:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,12 @@
         $arrayPostData['messages'][0]['text'] = "อนุตราสวัสดีจ้าาา";
         replyMsg($arrayHeader,$arrayPostData);
     }
+    else if ($message <> "สวัสดี") {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "อนุตราไม่เข้าใจค่ะ...?";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
     #ตัวอย่าง Message Type "Sticker"
     else if($message == "ฝันดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
